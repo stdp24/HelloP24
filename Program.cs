fde66a66acf1f0c70b251539dfd648ac6dce6d29
std <std@protection24.com>
2020-12-02 08:23:33 +0100

    Push solution

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloP24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello P24!");
+        }
+    }
+}
